234846a392cdb4bb797f8f68dc13ebe340ae5c3d
Felix Ruess <felix.ruess@gmail.com>
2012-10-24 22:49:23 +0200

    fix velocity sensor

--- src/morse/sensors/velocity.py
+++ src/morse/sensors/velocity.py
@@ -28,16 +28,13 @@ class VelocityClass(morse.core.sensor.MorseSensorClass):
         # make new references to the robot velocities and use those.
         self.robot_w = self.robot_parent.blender_obj.localAngularVelocity
         self.robot_v = self.robot_parent.blender_obj.localLinearVelocity
-        self.robot_world_v = self.robot_parent.blender_obj.localLinearVelocity
+        self.robot_world_v = self.robot_parent.blender_obj.worldLinearVelocity
 
         # get the transformation from robot to sensor frame
         (loc, rot, scale) = self.robot_parent.position_3d.transformation3d_with(self.position_3d).matrix.decompose()
         logger.debug("body2sensor rotation RPY [% .3f % .3f % .3f]" % tuple(degrees(a) for a in rot.to_euler()))
-        # store body to imu rotation and translation
+        # store body to sensor rotation
         self.rot_b2s = rot
-
-        # reference for rotating world frame to imu frame
-        self.rot_w2s = self.blender_obj.worldOrientation
 
         logger.info("Component initialized, runs at %.2f Hz", self.frequency)
 
@@ -45,13 +42,10 @@ class VelocityClass(morse.core.sensor.MorseSensorClass):
     def default_action(self):
         """ Get the linear and angular velocity of the blender object. """
 
-        # rot_b2i rotates body frame to imu frame
+        # rot_b2s rotates body frame to sensor frame
         # take the inverse rotation to transform a vector from body to sensor
-        rates = self.rot_b2s.inverted() * self.robot_w
-        vel = self.rot_b2s.inverted() * self.robot_v
-        world_vel = self.rot_w2s.inverted() * self.robot_world_v
 
         # Store the important data
-        self.local_data['linear_velocity'] = vel
-        self.local_data['angular_velocity'] = rates
-        self.local_data['world_linear_velocity'] = world_vel
+        self.local_data['linear_velocity'] = self.rot_b2s.inverted() * self.robot_v
+        self.local_data['angular_velocity'] = self.rot_b2s.inverted() * self.robot_w
+        self.local_data['world_linear_velocity'] = self.robot_world_v.copy()
