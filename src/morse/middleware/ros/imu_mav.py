f3073d43ab557a0d5d556681ba5e20bcec2449a4
Felix Ruess <felix.ruess@gmail.com>
2013-03-24 01:49:02 +0100

    [ros] add ImuMav publisher

---
+++ src/morse/middleware/ros/imu_mav.py
@@ -0,0 +1,28 @@
+import roslib; roslib.load_manifest('uav_msgs');
+from uav_msgs.msg import ImuMav
+from morse.middleware.ros import ROSPublisher
+
+class ImuMavPublisher(ROSPublisher):
+    """ Publish the data of the IMU sensor (without covariance). """
+    ros_class = ImuMav
+    default_frame_id = '/imu'
+
+    def default(self, ci='unused'):
+        imu = ImuMav()
+        imu.header = self.get_ros_header()
+
+        imu.angular_velocity.x = self.data['angular_velocity'][0]
+        imu.angular_velocity.y = self.data['angular_velocity'][1]
+        imu.angular_velocity.z = self.data['angular_velocity'][2]
+
+        imu.linear_acceleration.x = self.data['linear_acceleration'][0]
+        imu.linear_acceleration.y = self.data['linear_acceleration'][1]
+        imu.linear_acceleration.z = self.data['linear_acceleration'][2]
+
+        imu.magnetometer.x = 0.0
+        imu.magnetometer.y = 0.0
+        imu.magnetometer.z = 0.0
+
+        imu.baro_height = 0.0
+
+        self.publish(imu)
